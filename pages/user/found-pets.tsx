a89f426fff2e9550a54828ba46ca621532b197ba
Andrew Tran <andrewdaotran@gmail.com>
2022-07-08 22:00:56 -0700

    Change user/found-pets and user/lost-pets to files instead of folder/index

--- pages/user/found-pets.tsx
+++ pages/user/found-pets.tsx
@@ -1,33 +1,41 @@
 import React from 'react'
 import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
-import { addApolloState, initializeApollo } from '../../../apollo/apollo-client'
-import { USER_LOST_PETS_QUERY, USER_QUERY } from '../../../apollo/userQueries'
+import { addApolloState, initializeApollo } from '../../apollo/apollo-client'
+import { USER_FOUND_PETS_QUERY, USER_QUERY } from '../../apollo/userQueries'
 import { useQuery } from '@apollo/client'
 
-import { UserData } from '../../../typings'
-import PetQueryCard from '../../../components/PetQueryCard'
+import { PetData, UserData } from '../../typings'
+import PetQueryCard from '../../components/PetQueryCard'
+import { useRouter } from 'next/router'
 
 interface Props {
 	user: UserData
 }
 
-const UserLostPets = ({ user }: Props) => {
-	const { data: userQueryData } = useQuery(USER_LOST_PETS_QUERY, {
+const UserFoundPets = ({ user }: Props) => {
+	const { data: userQueryData } = useQuery(USER_FOUND_PETS_QUERY, {
 		variables: { sub: user.sub },
 	})
+
+	const router = useRouter()
+
+	// const handleNavigate = (petId) => {
+	// 	router.push(`/pets/found-pets/${petId}`)
+	// }
+
 	return (
 		<div className=''>
 			<h2 className='sm:text-7xl text-4xl text-center mt-6 mb-2 lg:mb-6'>
-				Your Lost Pets
+				Your Found Pets
 			</h2>
 			<div className='grid p-4 gap-6 mx-auto justify-items-center md:grid-cols-2 lg:flex lg:justify-center lg:gap-24'>
-				{userQueryData.user.lostPets.map((pet) => {
+				{userQueryData.user.foundPets.map((pet: PetData) => {
 					return (
 						<PetQueryCard
 							pet={pet}
 							key={String(pet.id)}
 							userPets={true}
-							navigateTo={`/pets/lost-pets/${pet.id}`}
+							navigateTo={`/pets/found-pets/${pet.id}`}
 						/>
 					)
 				})}
@@ -36,7 +44,7 @@ const UserLostPets = ({ user }: Props) => {
 	)
 }
 
-export default UserLostPets
+export default UserFoundPets
 
 export const getServerSideProps = withPageAuthRequired({
 	async getServerSideProps(context) {
@@ -50,7 +58,7 @@ export const getServerSideProps = withPageAuthRequired({
 		// })
 
 		const userFoundPets = await apolloClient.query({
-			query: USER_LOST_PETS_QUERY,
+			query: USER_FOUND_PETS_QUERY,
 			variables: { sub: auth0User.sub },
 		})
 
--- pages/user/lost-pets.tsx
+++ pages/user/lost-pets.tsx
@@ -1,41 +1,33 @@
 import React from 'react'
 import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
-import { addApolloState, initializeApollo } from '../../../apollo/apollo-client'
-import { USER_FOUND_PETS_QUERY, USER_QUERY } from '../../../apollo/userQueries'
+import { addApolloState, initializeApollo } from '../../apollo/apollo-client'
+import { USER_LOST_PETS_QUERY, USER_QUERY } from '../../apollo/userQueries'
 import { useQuery } from '@apollo/client'
 
-import { PetData, UserData } from '../../../typings'
-import PetQueryCard from '../../../components/PetQueryCard'
-import { useRouter } from 'next/router'
+import { UserData } from '../../typings'
+import PetQueryCard from '../../components/PetQueryCard'
 
 interface Props {
 	user: UserData
 }
 
-const UserFoundPets = ({ user }: Props) => {
-	const { data: userQueryData } = useQuery(USER_FOUND_PETS_QUERY, {
+const UserLostPets = ({ user }: Props) => {
+	const { data: userQueryData } = useQuery(USER_LOST_PETS_QUERY, {
 		variables: { sub: user.sub },
 	})
-
-	const router = useRouter()
-
-	// const handleNavigate = (petId) => {
-	// 	router.push(`/pets/found-pets/${petId}`)
-	// }
-
 	return (
 		<div className=''>
 			<h2 className='sm:text-7xl text-4xl text-center mt-6 mb-2 lg:mb-6'>
-				Your Found Pets
+				Your Lost Pets
 			</h2>
 			<div className='grid p-4 gap-6 mx-auto justify-items-center md:grid-cols-2 lg:flex lg:justify-center lg:gap-24'>
-				{userQueryData.user.foundPets.map((pet: PetData) => {
+				{userQueryData.user.lostPets.map((pet) => {
 					return (
 						<PetQueryCard
 							pet={pet}
 							key={String(pet.id)}
 							userPets={true}
-							navigateTo={`/pets/found-pets/${pet.id}`}
+							navigateTo={`/pets/lost-pets/${pet.id}`}
 						/>
 					)
 				})}
@@ -44,7 +36,7 @@ const UserFoundPets = ({ user }: Props) => {
 	)
 }
 
-export default UserFoundPets
+export default UserLostPets
 
 export const getServerSideProps = withPageAuthRequired({
 	async getServerSideProps(context) {
@@ -58,7 +50,7 @@ export const getServerSideProps = withPageAuthRequired({
 		// })
 
 		const userFoundPets = await apolloClient.query({
-			query: USER_FOUND_PETS_QUERY,
+			query: USER_LOST_PETS_QUERY,
 			variables: { sub: auth0User.sub },
 		})
 
