1d27d8b31e51c3a8187f1240b043133bdb63588d
Andrew Tran <andrewdaotran@gmail.com>
2022-08-14 15:15:22 -0700

    Change environment variables

--- pages/api/pet-finder-oauth-token.js
+++ pages/api/pet-finder-oauth-token.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 export default async (req, res) => {
 	const params = new URLSearchParams()
 	params.append('grant_type', 'client_credentials')
-	params.append('client_id', process.env.NEXT_PUBLIC_PET_FINDER_API_KEY)
-	params.append('client_secret', process.env.NEXT_PUBLIC_PET_FINDER_SECRET)
+	params.append('client_id', process.env.PET_FINDER_API_KEY)
+	params.append('client_secret', process.env.PET_FINDER_SECRET)
 
 	const petFinderRes = await axios.post(
 		'https://api.petfinder.com/v2/oauth2/token',
