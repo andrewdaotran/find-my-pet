f11079dbad3e6194a001b6ead5b25886083310d1
Andrew Tran <andrewdaotran@gmail.com>
2022-08-16 11:13:27 -0700

    Append https:// before using VERCEL_URL

--- pages/pets/pet-adoption/[adoptionId].tsx
+++ pages/pets/pet-adoption/[adoptionId].tsx
@@ -22,7 +22,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 	const host = context.req.headers.host
 	const adoptionId = context.query.adoptionId
 	const { data: accessToken } = await axios.get(
-		`${process.env.BASE_URL}/api/pet-finder-oauth-token`
+		`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/pet-finder-oauth-token`
+		// `${process.env.BASE_URL}/api/pet-finder-oauth-token`
 		// `http://${host}/api/pet-finder-oauth-token`
 
 		// `${process.env.VERCEL_URL}/api/pet-finder-oauth-token`
